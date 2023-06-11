265ce7ca63519108e8baca42b3c04f3c07ebc7b4
Pedro Souza <pedroisidorio1@gmail.com>
2023-06-11 16:42:26 -0300

    feat: creation of new screen for adding items

--- App.tsx
+++ App.tsx
@@ -1,39 +1,30 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
-import { Teste } from './src/controller/services/teste.service';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Home } from './src/views/pages/home';
+import { Adicionar } from './src/views/pages/adicionar';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const teste = new Teste()
-  const [values, setValues] = useState([])
-
-  function click(val: string) {
-teste.getTeste(val)
-  }
-
-  useEffect(() => {
-    teste.getAllTeste().then(
-      (resp) => setValues(resp)
-    )
-  }, [])
-
   return (
-    <View>
-      <Text>Hello World</Text>
-      {values.length != 0 &&
-        values.map(i => {
-          return (
-            <div key={i.id}>
-              <Text >{i.nome}</Text>
-              <button onClick={() => click(i.id)} >ação</button>
-            </div>
-          )
-        })
-      }
-    </View>
+    <NavigationContainer children={
+      <Stack.Navigator children={
+        <React.Fragment>
+          <Stack.Screen
+            name='Home'
+            options={{
+              headerTintColor: 'red'
+            }}
+            component={Home} />
+          <Stack.Screen
+            name='Adicionar'
+            options={{
+              headerTintColor: 'red'
+            }}
+            component={Adicionar} />
+        </React.Fragment>
+      } initialRouteName='Home' />
+    } />
   );
 }
---
+++ src/views/pages/adicionar.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View,Text } from 'react-native';
+
+export function Adicionar() {
+    return (
+        <View>
+            <Text> Adicionar </Text>
+        </View>
+    )
+}
+
+export default Adicionar;
